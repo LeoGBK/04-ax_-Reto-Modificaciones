a8870843d295c7c590e2269fe628088f5d4c5d70
L€0 <69213766+LeoGBK@users.noreply.github.com>
2025-02-26 00:08:52 +0100

    Ending JS code

--- script.js
+++ script.js
@@ -5,7 +5,14 @@ const texto = document.querySelector('p');
 const boton = document.querySelector('button');
 
 boton.onclick = function () {
-    // if (!texto.classList.contains('rojo')) {}
 
-    boton.style.backgroundColor = 'yellow';
+    if (texto.style.color == 'red')
+        texto.style.color = 'black';
+    else
+        texto.style.color = 'red';
+
+    if (circulo.style.backgroundColor == 'yellow')
+        circulo.style.backgroundColor = 'lightgreen';
+    else
+        circulo.style.backgroundColor = 'yellow';
 }
