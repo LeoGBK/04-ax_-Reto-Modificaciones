dbb51682f8945a230a0b5c57cd7b3a273f28d047
L€0 <69213766+LeoGBK@users.noreply.github.com>
2025-02-25 23:57:04 +0100

    Adding JS script content

---
+++ script.js
@@ -0,0 +1,11 @@
+const circulo = document.querySelector('div');
+
+const texto = document.querySelector('p');
+
+const boton = document.querySelector('button');
+
+boton.onclick = function () {
+    // if (!texto.classList.contains('rojo')) {}
+
+    boton.style.backgroundColor = 'yellow';
+}
