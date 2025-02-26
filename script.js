864335b97408d84acce1a7047837b6f8f55f61bb
L€0 <69213766+LeoGBK@users.noreply.github.com>
2025-02-26 11:38:27 +0100

    Comments added to HTML, CSS and JS files

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
+// Storing in "circulo" the element in <div> tag (circle).
 const circulo = document.querySelector('div');
 
+// Storing in "texto" the element in <p> tag.
 const texto = document.querySelector('p');
 
+// Storing in "boton" the element in <button> tag.
 const boton = document.querySelector('button');
 
+// This onclick event triggers a function that executes conditions to change values styles.
 boton.onclick = function () {
 
     if (texto.style.color == 'red')
